5d4d1248fdcd94ec2310e0a4aeacfa5db1f36083
vilela69 <frederico.vilelacurti@gmail.com>
2018-03-23 09:24:06 -0300

    elements done

--- elements.py
+++ elements.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 class Node():
   def __init__(self,n,x,y):
     self.n = n
@@ -5,14 +7,24 @@ class Node():
     self.y = y
 
 class Bar():
-  def __init__(self, startNode, endNode, group = None, material = None):
+  def __init__(self, id, startNode, endNode, group = None, material = None):
+    self.id = id
     self.startNode = startNode
     self.endNode = endNode
     self.group = group
     self.material = material
+
+  def show(self):
+    pprint(vars(self))
 
 class Material():
   def __init__(self, mde, tta, tca):
     self.mde = mde
     self.tta = tta
     self.tca = tca
+
+class Group():
+  def __init__(self, n, amount, elementType):
+    self.n = int(n)
+    self.amount = int(amount)
+    self.elementType = elementType
--- reader.py
+++ reader.py
@@ -1,34 +1,82 @@
-from elements import Node, Bar, Material
+from elements import Node, Bar, Material, Group
+from pprint import pprint
 
 class Reader():
   def read(self, filePath):
-    result = {}
-
+    result = {
+      'coordinates': {
+        'count': 0,
+        'nodes': []
+      },
+      'element_groups': {
+        'number_of_elements': 0,
+        'count': 0,
+        'groups': []
+      },
+      'bars': [],
+    }
+    # print(result['coordinates']['nodes'])
+    
     with open(filePath,'r') as f:
       lines = f.readlines()
-      # while (line):
+      elementCounter = 0
+
       for line in lines:
         line = line.strip()
-        print(line)
+        el = line.split(' ')
+        
+        if len(line) == 0:
+          continue
 
         if len(line) != 0 and line[0] == "*":
           section = line[1:].lower()
-          result[section] = {}
           continue
         
         if section == 'coordinates':
-          coord = line.split(' ')
-          if len(coord) == 1:
-            result[section]['count'] = coord[0]
-            result[section]['nodes'] = []
+          if len(el) == 1 :
+            result[section]['count'] = el[0]
           else:
-            print("appending node")
-            result[section]['nodes'].append(Node(coord[0],coord[1],coord[2]))
+            result[section]['nodes'].append(Node(int(el[0]), float(el[1]), float(el[2])))
             
+        if section == 'element_groups':
+          if len(line) == 1:
+            result[section]['count'] = int(el[0])
+          else:
+            result[section]['groups'].append(Group(el[0], el[1], el[2]))
+            result[section]['number_of_elements'] += int(el[1])
 
-        elif section == 'element_groups':
-            eg = line.split(' ')
-            print(eg)
+        elif section == 'incidences':
+          groups = result['element_groups']['groups']
+          nodes = result['coordinates']['nodes']
+          currentGroupAmount = groups[elementCounter].amount
+
+          if elementCounter < result['element_groups']['number_of_elements']:
+            if (currentGroupAmount > 0):
+              bar = Bar(el[0], nodes[int(el[1])-1], nodes[int(el[2])-1], groups[elementCounter])
+              result['bars'].append(bar)
+              currentGroupAmount -= 1
+              bar.show()
+            elementCounter += 1
+
+         
+          # if len(allAmounts) == 0:
+          #   for i in range(result['element_groups']['count']):
+          #     allAmounts.append(groups[i].amount)
+          #   print(allAmounts)
+
+          # for j in range(len(allAmounts)):
+
+
+
+    pprint(result)
+          
+    
+    
+          
+
+
+
+            # print(eg)
             
 # line.find("E") != -1
           # else:
@@ -37,7 +85,6 @@ class Reader():
           #   print("found number",num)
         
 
-        print(result)
 
 
 
