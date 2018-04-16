1af871794e6ac55a37a44eb10b206e5b1fc16a7a
raphacosta27 <rapha2706@hotmail.com>
2018-04-15 21:48:41 -0300

    Solver pronto e subtituido no calculator

--- solver.py
+++ solver.py
@@ -6,7 +6,7 @@ class Solver():
     self.ite = ite
     self.tol = tol
     self.k = k
-    self.f = f    
+    self.f = f
 
   def gauss(self):
     u = []
@@ -19,7 +19,7 @@ class Solver():
     for j in range(self.ite):
       print('----- Iteração {} -----'.format(j))
       maior_erro = 0
-      
+
 
       for l in range(height):
         temp = self.f[l] / self.k[l][l]
@@ -28,25 +28,23 @@ class Solver():
         for w in range(height):
           if (w != l):
             temp -= ((self.k[l][w] * u[w]) / (self.k[l][l]))
-          
+
         if (temp == 0):
           erro = abs(temp - u[l])
         else:
-          erro = abs((temp - u[l])/temp) 
-
-        if (erro > maior_erro):
-          maior_erro = erro
+          erro = abs((temp - u[l])/temp)
 
         u[l] = temp
-        # print('e:',erro)
 
-      print(u)
-      if (maior_erro <= self.tol):
-        return u, maior_erro
-        ##u[l] = temp
+        if(erro > 0):   
+          if (erro > maior_erro):
+            maior_erro = erro
+
+            if (maior_erro <= self.tol):
+              return u, maior_erro
+
     return u, maior_erro
 
-        #u[k] = ( self.f[k[k][height] / k[height][0] ) * u[k+1]] / k[0][k] ) - ( k
 
   def jacobi(self):
     u = []
@@ -58,49 +56,47 @@ class Solver():
 
     for j in range(self.ite):
       print('----- Iteração {} -----'.format(j))
-      maior_erro = 0
-      uite = u
-
+      maior_erro = -1
+      uAux = []
+      for i in range(len(u)):
+          uAux.append(u[i])
+      
+    
       for l in range(height):
+        # print("uite: ", uAux)
         temp = self.f[l] / self.k[l][l]
 
         for w in range(height):
           if (w != l):
-            temp -= ((self.k[l][w] * uite[w]) / (self.k[l][l]))
+            temp -= ((self.k[l][w] * uAux[w]) / (self.k[l][l]))
 
         u[l] = temp
- 
+
         if(j > 0):
           if (temp == 0):
-            print("alo")
-            print(uite[l])
-            erro = abs((temp - uite[l]))
+            erro = abs((temp - uAux[l]))
           else:
-            erro = abs((temp - uite[l])/temp)
+            erro = abs((temp - uAux[l])/temp)
 
-          print("erro: ",erro)
+          if(erro > 0):      
+            if (erro > maior_erro):
+              maior_erro = erro
 
-          if (erro > maior_erro):
-            maior_erro = erro
+            if (maior_erro <= self.tol):
+              return u, maior_erro
 
-
-      print('maior erro: ',maior_erro)
-
-      if (maior_erro <= self.tol):
-        return u, maior_erro
-        ##u[l] = temp
     return u, maior_erro
 
 
-k = np.array([[3, -1, 0, 0], [-1, 3, 0, -2], [0, 0, 3, 1], [0, -2, 1, 3]])
-k = k * 5 * (10 ** 4)
-f = [0,0,0,-100]
+# k = np.array([[3, -1, 0, 0], [-1, 3, 0, -2], [0, 0, 3, 1], [0, -2, 1, 3]])
+# k = k * 5 * (10 ** 4)
+# f = [0,0,0,-100]
 # print(k)
-solver = Solver(600, 10**-9, k, f)
-print(k)
-teste = solver.jacobi()
-print("Matriz de Us: ", teste[0])
-print("Erro: ", teste[1])
+# solver = Solver(600, 10**-9, k, f)
+# print(k)
+# teste = solver.jacobi()
+# print("Matriz de Us: ", teste[0])
+# print("Erro: ", teste[1])
 # teste = [x for el in teste]
 
 
